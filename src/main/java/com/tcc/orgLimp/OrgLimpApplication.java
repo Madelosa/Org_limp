6e93c3f503555419b0c1d1201c82e5dbd5c0e1dd
Gustavo Aparecido da Silva <guch4n@hotmail.com>
2026-08-27 19:40:43 -0300

    Trazer o projeto para estrutura spring (java) Fixes #24

---
+++ src/main/java/com/tcc/orgLimp/OrgLimpApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.orgLimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgLimpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgLimpApplication.class, args);
+	}
+
+}
