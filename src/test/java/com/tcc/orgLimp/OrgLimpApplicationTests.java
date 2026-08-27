09aaf0a7d4c71d673a0627cba768bcb2e68598a8
Gustavo Aparecido da Silva <139185842+guch4n@users.noreply.github.com>
2026-08-27 19:42:18 -0300

    Trazer o projeto para estrutura spring (java) (#26)
    Fixes #24

---
+++ src/test/java/com/tcc/orgLimp/OrgLimpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.orgLimp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgLimpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
